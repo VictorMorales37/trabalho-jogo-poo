8f030cef64b75cd793ea4847b164a33930295958
MMullerdR <miguelmullerdarosa@gmail.com>
2026-06-24 09:49:45 -0300

    concertei algumas das alterações de ontem

--- src/sistema/SistemaCombate.java
+++ src/sistema/SistemaCombate.java
@@ -1,7 +1,11 @@
 package sistema;
+import sistema.movimentacao.ResultadoMovimento;
+import entidades.Dinossauro;
 import entidades.Jogador;
 import util.Macros;
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class SistemaCombate {
     private static final Random RANDOM = new Random();
@@ -46,43 +50,50 @@ public class SistemaCombate {
             return 0;
         }
     }
-    /* AAAAAAAAAAAA (aqui falta implementar muita coisa -> resultado movimento virar um dinossauro especifico do mapa)
-     *
-     *    public void comecarCombate(ResultadoMovimento resultadoMovimento) {
-     *        Dinossauro alvo = encontrarDinossauroPorResultado(resultadoMovimento);
-     *        if (alvo == null) return;
-     *
-     *        executarCombate(jogador, alvo);
-     *    }
-     */
-
+    // AAAAAAAAAAAA (aqui falta implementar muita coisa -> resultado movimento virar um dinossauro especifico do mapa)
     /*
-        private void executarCombate(Jogador jogador, Dinossauro alvo) {
-            while (jogador.getSaude() > 0 && alvo.getSaude() > 0) {
-
-                menu.opcoesCombate(); // 1 - mão | 2 - bastão | 3 - dardos | 4 - curar | 5 - fugir
-                leitorDeInput.input = leitorDeInput.lerInput(1, 5);
-
-                int dano = switch (leitorDeInput.input) {
-                    case 1 -> sistemaCombate.atacarMao();
-                    case 2 -> sistemaCombate.atacarBastao(jogador);
-                    case 3 -> sistemaCombate.atacarArmaDardos(jogador);
-                    case 4 -> { sistemaCombate.curar(jogador); yield 0; } // yield é o return do switch
-                    case 5 -> { sistemaCombate.fugir; yied 0 }
-                    default -> 0;
-                };
-
-                alvo.setSaude(alvo.getSaude() - dano);
-                // combate com dinossauro Rex é diferente
-                if (!passouTestePercepcao(jogador)) { jogador.setSaude(jogador.getSaude() - 1); }
-            }
-            // Falta implementar:
-            // remover os dinossauros do mapa e mover o jogador
-            // combate com dinossauro Rex
-            //
-
+    public Dinossauro encontrarDinossauroPorResultado(ResultadoMovimento resultadoMovimento) {
+        switch (resultadoMovimento) {
+            case ResultadoMovimento.ENCONTROU_COMPSOGNATO -> return Dinossauro.
         }
-     */
+    }
+    */
+    /*
+    public void comecarCombate(ResultadoMovimento resultadoMovimento, Jogador jogador, Scanner scanner) {
+         Dinossauro alvo = encontrarDinossauroPorResultado(resultadoMovimento);
+         if (alvo == null) return;
+
+         executarCombate(jogador, alvo, scanner);
+    }
+    */
+    private void executarCombate(Jogador jogador, Dinossauro alvo, Scanner scanner) {
+        Menu menu = new Menu();
+        LeitorDeInput leitorDeInput = new LeitorDeInput(scanner);
+
+        while (jogador.getSaude() > 0 && alvo.getSaude() > 0) {
+            menu.opcoesCombate(); // 1 - mão | 2 - bastão | 3 - dardos | 4 - curar | 5 - fugir
+            leitorDeInput.input = leitorDeInput.lerInput(1, 5);
+
+            int dano = switch (leitorDeInput.input) {
+                case 1 -> atacarMao();
+                case 2 -> atacarBastao(jogador);
+                case 3 -> atacarArmaDardos(jogador);
+                case 4 -> { curar(jogador); yield 0; } // yield é o return do switch
+                case 5 -> { fugir(); yield 0; }
+                default -> 0;
+            };
+
+            alvo.setSaude(alvo.getSaude() - dano);
+            // combate com dinossauro Rex é diferente
+            if (!passouTestePercepcao(jogador)) { jogador.setSaude(jogador.getSaude() - 1); }
+        }
+        // Falta implementar:
+        // remover os dinossauros do mapa e mover o jogador
+        // combate com dinossauro Rex
+        // implementar funcao fugir
+
+    }
+
     // ----------------------------- cura -----------------------------
     public void acharKitsMedicos(Jogador j) {
         j.setKitsMedicos(j.getKitsMedicos() + 1);
@@ -102,5 +113,10 @@ public class SistemaCombate {
     public boolean passouTestePercepcao(Jogador j) {
         int dado = RANDOM.nextInt(3) + 1;
         return dado <= j.getPercepcao();
+    }
+    // ----------------------------- fugir -----------------------------
+
+    public void fugir(){
+        return;
     }
 }
